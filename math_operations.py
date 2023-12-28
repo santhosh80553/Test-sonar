cbd9f3c5fede0bc615723302b07f4486ea96b4af
santhosh80553 <107468516+santhosh80553@users.noreply.github.com>
2023-12-29 01:57:38 +0530

    Update math_operations.py

--- math_operations.py
+++ math_operations.py
@@ -1,7 +1,10 @@
-# math_operations.py
+# calculator.py
 
 def add(x, y):
-    return x + y
+    result = x + y
+    if result > 10:
+        print("Result is greater than 10!")
+    return result
 
 def subtract(x, y):
     return x - y
@@ -13,3 +16,6 @@ def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero")
     return x / y
+
+def is_positive(x):
+    return x > 0
